9bb14cee60e5c622d21f04a1aa8137a0c80f2608
iridescentu <jihee.yoon.119@gmail.com>
2024-06-30 20:57:37 +0900

    Video Repository updated

---
+++ pado-ing/src/main/java/com/sparta/padoing/repository/VideoRepository.java
@@ -0,0 +1,9 @@
+package com.sparta.padoing.repository;
+
+import com.sparta.padoing.model.Video;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VideoRepository extends JpaRepository<Video, Long> {
+}
