3c5a0320d885cf3165200d396836f9c8fd2a4192
iridescentu <jihee.yoon.119@gmail.com>
2024-07-15 14:05:11 +0900

    TOP 5 조회 날짜 계산 로직 수정

--- pado-ing/src/main/java/com/sparta/padoing/controller/VideoStatsController.java
+++ pado-ing/src/main/java/com/sparta/padoing/controller/VideoStatsController.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.Map;
 
@@ -52,15 +53,29 @@ public class VideoStatsController {
         return videoStatsService.getTop5VideosByPlayTime(user.getId(), startDate, endDate);
     }
 
+//    private LocalDate calculateStartDate(String period, LocalDate endDate) {
+//        System.out.println("Controller: calculateStartDate called with Period: " + period + ", EndDate: " + endDate);
+//        switch (period) {
+//            case "1일":
+//                return endDate.minusDays(1);
+//            case "1주일":
+//                return endDate.minusWeeks(1);
+//            case "1달":
+//                return endDate.minusMonths(1);
+//            default:
+//                throw new IllegalArgumentException("Invalid period: " + period);
+//        }
+//    }
+
     private LocalDate calculateStartDate(String period, LocalDate endDate) {
         System.out.println("Controller: calculateStartDate called with Period: " + period + ", EndDate: " + endDate);
         switch (period) {
             case "1일":
-                return endDate.minusDays(1);
+                return endDate; // 오늘 하루만 포함
             case "1주일":
-                return endDate.minusWeeks(1);
+                return endDate.with(DayOfWeek.MONDAY); // 해당 주의 월요일부터 포함
             case "1달":
-                return endDate.minusMonths(1);
+                return endDate.withDayOfMonth(1); // 해당 달의 1일부터 포함
             default:
                 throw new IllegalArgumentException("Invalid period: " + period);
         }
