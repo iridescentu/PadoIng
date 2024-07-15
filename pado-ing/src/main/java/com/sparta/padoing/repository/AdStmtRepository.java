cbe8451b6fa0da8433bedcb306c600a3ecd233ae
iridescentu <jihee.yoon.119@gmail.com>
2024-07-15 18:11:44 +0900

    updated

--- pado-ing/src/main/java/com/sparta/padoing/repository/AdStmtRepository.java
+++ pado-ing/src/main/java/com/sparta/padoing/repository/AdStmtRepository.java
@@ -1,14 +1,32 @@
+//package com.sparta.padoing.repository;
+//
+//import com.sparta.padoing.model.AdStmt;
+//import com.sparta.padoing.model.id.AdStmtId;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//import java.time.LocalDate;
+//import java.util.List;
+//
+//@Repository
+//public interface AdStmtRepository extends JpaRepository<AdStmt, AdStmtId> {
+//    List<AdStmt> findByVideoAd_Video_User_IdAndDateBetween(Long userId, LocalDate startDate, LocalDate endDate);
+//}
+
 package com.sparta.padoing.repository;
 
 import com.sparta.padoing.model.AdStmt;
+import com.sparta.padoing.model.VideoAd;
 import com.sparta.padoing.model.id.AdStmtId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface AdStmtRepository extends JpaRepository<AdStmt, AdStmtId> {
     List<AdStmt> findByVideoAd_Video_User_IdAndDateBetween(Long userId, LocalDate startDate, LocalDate endDate);
+    Optional<AdStmt> findByVideoAdAndDate(VideoAd videoAd, LocalDate date); // 추가된 메서드
 }
